cd9ffceff750448a415c34cee4d81acda650b1ff
Frank <franklinjalves@gmail.com>
2023-10-29 09:57:07 +0000

    refactor: simplify project

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,7 +1,7 @@
 import { type MetaFunction } from '@remix-run/node'
 
 export const meta: MetaFunction = () => {
-  return [{ title: 'Remix Starter Template' }, { name: 'description', content: 'Remix Starter Template' }]
+  return [{ title: 'Spotify 2008' }, { name: 'description', content: 'Spotify 2008' }]
 }
 
 export default function Index() {
@@ -9,9 +9,7 @@ export default function Index() {
     <main>
       <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
         <div className="text-center">
-          <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-            Remix Starter Template
-          </h1>
+          <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Spotify 2008</h1>
           <p className="mt-6 text-lg leading-8 text-gray-600">
             Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit
             sunt amet fugiat veniam occaecat fugiat aliqua.
