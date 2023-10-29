603b302a8bff9cadf124cdda4c0dc869c9d30c14
Frank <franklinjalves@gmail.com>
2023-10-29 20:02:45 +0000

    refactor: remove unused refresh access token

--- app/lib/spotify.server.ts
+++ app/lib/spotify.server.ts
@@ -1,6 +1,5 @@
 import { SpotifyApi } from '@spotify/web-api-ts-sdk'
 
-// Choose one of the following:
 export const spotify = SpotifyApi.withClientCredentials(
   process.env.SPOTIFY_CLIENT_ID as string,
   process.env.SPOTIFY_CLIENT_SECRET as string,
